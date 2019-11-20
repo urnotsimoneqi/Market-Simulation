ce78554f4d5d25cdd54a35fa2e1c6febee12e3f0
matthewzhangx <matthewzhangx@github.com>
2019-11-20 10:36:17 +0800

    Create related_products.py

---
+++ related_products.py
@@ -0,0 +1,8 @@
+class Related_Products:
+    def __init__(self, name, related_products, discount=1):
+        self.name = name
+        self.related_products = related_products
+        self.num_of_items = len(related_products)
+        self.amount = 0
+        for product in related_products:
+            self.amount += product.price * discount
