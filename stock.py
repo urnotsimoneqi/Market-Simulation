f3bd8b7ffa148a0785526789eb0c251e5cd379ff
matthewzhangx <matthewzhangx@github.com>
2019-11-22 11:38:06 +0800

    Add Product Market Price and add product quality to stock

--- stock.py
+++ stock.py
@@ -1,7 +1,9 @@
 class Stock(object):
-    def __init__(self, product_id, product_name, product_quality, product_status, seller_id, stock_quantity, stock_cost, stock_price):
+    def __init__(self, product_id, product_name, product_market_price, product_quality, product_status, seller_id,
+                 stock_quantity, stock_cost, stock_price):
         self.product_id = product_id
         self.product_name = product_name
+        self.market_price = product_market_price
         self.product_quality = product_quality
         self.product_status = product_status
         self.seller_id = seller_id
