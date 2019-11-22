8d274526df7d99f19adcafe5d02a95d799a1fbc1
matthewzhangx <matthewzhangx@github.com>
2019-11-22 11:37:41 +0800

    Add Product Market Price

--- product.py
+++ product.py
@@ -1,9 +1,9 @@
 class Product(object):
-    def __init__(self, product_id, name, price, quality, quantity):
+    def __init__(self, product_id, name, market_price, quality, quantity):
         assert quality <= 1
 
         self.product_id = product_id
         self.name = name
-        self.price = price
+        self.market_price = market_price
         self.quality = quality
         self.quantity = quantity  # num of products
